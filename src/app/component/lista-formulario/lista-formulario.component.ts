daf9fdb4a359ead18269eccf7fc01da2f51faef8
andresfalcon20 <andresfalcon.18@hotmail.com>
2025-07-30 10:11:44 -0500

    taller-fase2

---
+++ src/app/component/lista-formulario/lista-formulario.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-formulario',
+  standalone: true,
+  imports: [],
+  templateUrl: './lista-formulario.component.html',
+  styleUrl: './lista-formulario.component.css'
+})
+export class ListaFormularioComponent {
+
+}
